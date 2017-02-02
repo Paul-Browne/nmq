6eb07c9ff4740344dd28b93d9e61e4e5b9242397
Paul Browne <paul.browne@frantic.com>
2017-02-02 10:32:34 +0200

    Update nmmq.js

--- nmmq.js
+++ nmmq.js
@@ -32,17 +32,21 @@
     // kind of a mobile first approach, add another class name when breakpoint is reached
     // a more accurate naming convention would be something like tablet-and-up but, meh
     
-    // 670px - basically all phones in portrait and landscape
-    // the reason it is 670px is that the iphone 6, 6s and 7 are 667px wide in landsapce
+    // 672px - basically all phones in portrait and landscape
+    // the reason it is 672px is that the iphone 6, 6s and 7 are 667px wide in landsapce
     // It will also grab some 7 inch tablets (probably desirable)
-    // 1040 will grab most tablets (all ipads) in both portrait and landscape
-    // some really high DPI tablets (1280px), in landscape, will come under laptop
+    // 672 to 1296px will grab all tablets (all ipads) in both portrait and landscape
+    // 1920 / 1.5 = 1280 - which is usually what FullHD tablets get scaled to
+    // It will also grab 13" MacBooks, which is a personal preference of mine
+    // 1296 - 1472px may be a small range, but it includes the most popular 15.6" (1366px) laptops
+    // greater than 1472px grabs the 1600px wide monitors, and the FullHD 1920px ones
+    // 42em, 81em, 92em
     
-    if(windowWidth > 670) {
+    if(windowWidth > 672) {
       screenType = " tablet";
-      if(windowWidth > 1040){
+      if(windowWidth > 1296){
         screenType += " laptop";
-        if(windowWidth > 1440){
+        if(windowWidth > 1472){
           screenType += " desktop";
         }
       }
