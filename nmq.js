e528b567ddf656ae0080b6b3f78683a01388c48e
paulbrowne <Paulmatthewbrowne@gmail.com>
2019-01-16 15:18:59 +0200

    Update nmq.js

--- nmq.js
+++ nmq.js
@@ -6,7 +6,7 @@
         windowWidth > 720 ? html.add("tablet") : html.remove("tablet");
         windowWidth > 1280 ? html.add("laptop") : html.remove("laptop");
         windowWidth > 1560 ? html.add("desktop") : html.remove("desktop");
-        windowWidth > window.innerHeight ? html.add("landscape") : html.remove("landscape");
+        windowWidth < window.innerHeight ? html.add("portrait") : html.remove("portrait");
     }init();
     window.addEventListener("resize", function(){
         init();
